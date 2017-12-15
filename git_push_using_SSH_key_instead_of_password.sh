adf93858ba25f78cb41f4df5af4124a48e8537b2
kyberdrb <kyberdrb@gmail.com>
2017-12-15 18:34:51 +0100

    github SSH key autentification

---
+++ git_push_using_SSH_key_instead_of_password.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+LOCAL_MAIN_GITHUB_DIR=$1
+
+for repository in $(find $LOCAL_MAIN_GITHUB_DIR -maxdepth 1 -type d)
+do
+
+  REPO_URL=$(git -C $repository remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p')
+  if [ -z "$REPO_URL" ]; then
+    echo "-- ERROR:  Could not identify Repo url."
+    echo "   It is possible this repo is already using SSH instead of HTTPS."
+    continue
+  fi
+
+  USER=$(echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p')
+  if [ -z "$USER" ]; then
+    echo "-- ERROR:  Could not identify User."
+    continue
+  fi
+
+  REPO=$(echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p')
+  if [ -z "$REPO" ]; then
+    echo "-- ERROR:  Could not identify Repo."
+    continue
+  fi
+
+  NEW_URL="git@github.com:$USER/$REPO.git"
+  echo "Changing repo url from "
+  echo "  '$REPO_URL'"
+  echo "      to "
+  echo "  '$NEW_URL'"
+
+  CHANGE_CMD="git -C $repository remote set-url origin $NEW_URL"
+  $($CHANGE_CMD)
+
+  echo "Success"
+  echo
+
+done
